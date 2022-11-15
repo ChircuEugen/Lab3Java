7392011f438c95400406c54b616057d16a9b45bb
IDWebUser <113848433+IDWebUser@users.noreply.github.com>
2022-11-15 13:23:56 +0200

    Update RandomQuotesControllerTest.java

--- src/test/java/com/octopus/RandomQuotesControllerTest.java
+++ src/test/java/com/octopus/RandomQuotesControllerTest.java
@@ -4,7 +4,7 @@ import com.octopus.repository.AuthorRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,7 +12,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@SpringBootTest
+@SpringBootTest
 @AutoConfigureMockMvc
 public class RandomQuotesControllerTest {
 
