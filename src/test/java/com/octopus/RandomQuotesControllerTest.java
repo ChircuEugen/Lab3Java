b3bbcabdf5a3759bc91badcaaef9580e6f73c16b
IDWebUser <113848433+IDWebUser@users.noreply.github.com>
2022-11-15 21:31:26 +0200

    Update RandomQuotesControllerTest.java

--- src/test/java/com/octopus/RandomQuotesControllerTest.java
+++ src/test/java/com/octopus/RandomQuotesControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
