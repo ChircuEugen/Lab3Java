02ae3cbd9c7d5662b31603e7cb0f97ea7f0ca180
IDWebUser <113848433+IDWebUser@users.noreply.github.com>
2022-11-18 15:34:46 +0200

    Update RandomQuotesControllerTest.java

--- src/test/java/com/octopus/RandomQuotesControllerTest.java
+++ src/test/java/com/octopus/RandomQuotesControllerTest.java
@@ -5,6 +5,7 @@ import com.octopus.repository.QuoteRepository;
 import com.octopus.entity.Author;
 import com.octopus.RandomQuotesController;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -52,7 +53,7 @@ public class RandomQuotesControllerTest {
    // }
     @Test
     public void checkID(){
-        Author author = authorRepository.findById(3);
+        Author author = authorRepository.findById(3L);
         assertEquals("Jonathan", author.getFirstName());
     }
 }
